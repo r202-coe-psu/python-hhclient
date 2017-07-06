432d41dec6d6fdad48650fdfa97cba0698da9862
Thanathip Limna <boatkrap@gmail.com>
2017-07-06 15:33:14 -0500

    add deactivate application

--- hhclient/buildings.py
+++ hhclient/buildings.py
@@ -9,13 +9,19 @@ class BuildingManager(base.Manager):
     __resource_class__ = Building
     __resource_url__ = '/buildings'
 
-    def active_application(self, building_id, application_id):
+    def activate_application(self, building_id, application_id):
         url = '{}/{}/applications'.format(self.__resource_url__,
-                                       building_id)
+                                          building_id)
 
         data = dict(id=application_id)
         app_data = self.api.applications.schema.dump(data).data
 
         response, status_code = self.api.http_client.put(url,
-            data=app_data)
+                data=app_data)
 
+    def deactivate_application(self, building_id, application_id):
+        url = '{}/{}/applications/{}'.format(self.__resource_url__,
+                                             building_id,
+                                             application_id)
+
+        response, status_code = self.api.http_client.delete(url)
